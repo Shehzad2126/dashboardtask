313fb6b49d518645545a4c56092e657f831dc4c0
Shehzad Ali <shahzadaliarain2126@gmail.com>
2024-11-01 15:46:37 +0500

    "Removed RightSidebar component from MainContainer"

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -3,15 +3,9 @@ import MainContent from "./MainContent";
 const MainContainer = () => {
   return (
     <div className="flex bg-customGray">
-      {/* Main Content Area */}
       <div className="flex-1 w-full py-6">
         <MainContent />
       </div>
-
-      {/* Right Sidebar */}
-      {/* <div className="w-5/12 py-6">
-        <RightSidebar />
-      </div> */}
     </div>
   );
 };
